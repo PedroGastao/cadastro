c510eb97cc5789e64f14227ed1b6ec042d96b3d6
Pedro Gastão <pedro.gastao.14@gmail.com>
2022-05-05 23:07:57 -0300

    Adicionando medidas de validação de dados

---
+++ script.js
@@ -0,0 +1,10 @@
+let email = document.getElementById('email');
+
+email.addEventListener("input", function(event){
+    if(email.validity.typeMismatch){
+        email.setCustomValidity("campo obrigatorio");
+        email.reportValidity();
+    }else{
+        email.setCustomValidity("");
+    }
+});
